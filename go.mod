1d079f61d680de9197ea4b5e362f0b13eccec009
川野勇也 <kawanoisamunari@kawanoisamunarinoMacBook-Pro.local>
2022-11-12 05:36:55 +0900

    gin

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/yuya-kwn/go-api
-
-go 1.19
--- src/main.go
+++ src/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
-	fmt.Println("Hello,World!")
+	engine := gin.Default()
+	engine.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello,World!",
+		})
+	})
+	engine.Run(":8000")
 }
